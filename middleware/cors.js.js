72a820c49aaccfa151e02a96ba8dabbab1d74330
ImmortalAl <cooldude1343@yahoo.com>
2025-06-19 01:23:52 -0500

    Fix CORS configuration for local development

--- middleware/cors.js.js
+++ middleware/cors.js.js
@@ -2,7 +2,10 @@
 const allowedOrigins = [
   'https://immortalal.github.io',
   'https://mlnf.net',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'http://localhost:8080',
+  'http://127.0.0.1:3000',
+  'http://127.0.0.1:8080'
 ];
 
 module.exports = cors({
