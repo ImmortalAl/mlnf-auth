f8a7f079290d336ff0908b4f359c044d08045804
ImmortalAl <cooldude1343@yahoo.com>
2025-07-27 12:48:19 -0500

    Add immortal.u and IPFS gateways to CORS allowed origins

--- middleware/cors.js.js
+++ middleware/cors.js.js
@@ -5,7 +5,11 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:8080',
   'http://127.0.0.1:3000',
-  'http://127.0.0.1:8080'
+  'http://127.0.0.1:8080',
+  'https://immortal.u',
+  'https://cloudflare-ipfs.com',
+  'https://ipfs.io',
+  'https://gateway.ipfs.io'
 ];
 
 module.exports = cors({
